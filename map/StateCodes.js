be3be531d97d1bfb2992830583069a8b5f78ab97
Le Anh Vu <149586539+ki133@users.noreply.github.com>
2024-05-26 19:40:05 +1000

    Add files via upload

---
+++ map/StateCodes.js
@@ -0,0 +1,10 @@
+var StateCodes = {
+    "New South Wales": "NSW",
+    "Victoria": "VIC",
+    "Queensland": "QLD",
+    "South Australia": "SA",
+    "Western Australia": "WA",
+    "Tasmania": "TAS",
+    "Northern Territory": "NT",
+    "Australian Capital Territory": "ACT"
+};
